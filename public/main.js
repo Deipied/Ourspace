f3f7b2072041e2711968c98e4b1b22283662b17e
GroovyCj <hairston917@gmail.com>
2022-06-14 12:25:58 -0700

    added a function to read data and to attempt to edit code

--- public/main.js
+++ public/main.js
@@ -1,35 +1,73 @@
-document.querySelector("#form-submit").addEventListener("click", (event) => {
-  event.preventDefault();
-  createBlogEntry();
-});
-
-function createBlogEntry() {
-  const { nameInfo, textInfo } = readData();
+function formCard(event) {
   const card = document.createElement("div");
-  card.setAttribute("class", "blog-card");
+  card.setAttribute("class", "card");
 
   card.innerHTML = `
-  <h4> ${nameInfo} </h4>
-  <p> ${textInfo} </p>
-  
+  <% for(var i = 0; i < posts.length; i++) {%>
+  <h4><%= posts[i].title %></h4>
+  <p> <%= posts[i].thoughts %></p>
+  <p><%= posts[i]._id %> </p>
+  <% } %>
   `;
+
+  const buttonHolder = document.createElement("div");
+  buttonHolder.setAttribute("class", "buttons_holder");
+
+  const cardEditBtn = document.createElement("button");
+  cardEditBtn.innerText = "Edit";
+  cardEditBtn.addEventListener("click", editBlogEntry);
+
+  const cardDeleteBtn = document.createElement("button");
+  cardDeleteBtn.innerText = "Delete";
+  cardDeleteBtn.onclick = deleteBlogEntry;
+
+  buttonHolder.appendChild(cardEditBtn);
+  buttonHolder.appendChild(cardDeleteBtn);
+
+  card.appendChild(buttonHolder);
   document.querySelector("#msg-container-1").appendChild(card);
 }
 
-function readData() {
-  const nameElt = document.querySelector("#name-input");
-  const textElt = document.querySelector("#text-input");
+function editBlogEntry(event) {
+  const newTitle = window.prompt("Please enter a new Title for your post.");
+  const newThoughts = window.prompt("Please enter a new post.");
 
-  return {
-    nameInfo: nameElt.value,
-    textInfo: textElt.value,
-  };
+  const data = { title: newTitle, thoughts: newThoughts };
+  const url = event.target.id;
+  const concat = "'" + url + "'";
+
+  fetch("/posts/_id=" + concat, {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ data }),
+  })
+    .then((response) => response.json())
+    .then((respo) => {
+      window.location.reload(true);
+    });
+
+  /* fetchings data from the surver then uploads the new data
+need to add a prompt to hold the variables to have the user input the information. 
+might need to use the this keyword to get the data 
+
+*/
 }
 
-function newDate() {
-  // TODO: optional add a place to place a profile picture.
-  // TODO: timestamp the date from the program/ store timestamp
-  // * new Date() for getting timestamp to store in the database
+function deleteBlogEntry() {
+  // sends a delete request using the this key to grab the information of the element that called the fucntioned
 }
 
-// momentjs for friendly manipulating front end
+function serializer() {}
+
+// function readData() {
+//   const nameElt = document.querySelector("#name-input");
+//   const textElt = document.querySelector("#text-input");
+
+//   return {
+//     nameInfo: nameElt.value,
+//     textInfo: textElt.value,
+//   };
+// }
+
+//function newDate() {
+// TODO: optional add a place to pla
