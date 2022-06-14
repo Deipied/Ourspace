06da0df2ea474557116127d711f328c16153579e
Hoon <hoonhyungcho@gmail.com>
2022-06-14 17:16:50 -0500

    made working delete button, with changes to edit

--- public/main.js
+++ public/main.js
@@ -28,25 +28,23 @@ function formCard(event) {
   document.querySelector("#msg-container-1").appendChild(card);
 }
 
-console.log("Hello World");
+// console.log(posts);
 
 function editBlogEntry(event) {
   const newTitle = window.prompt("Please enter a new Title for your post.");
   const newThoughts = window.prompt("Please enter a new post.");
 
-  const data = { title: newTitle, thoughts: newThoughts };
-  const url = event.target.id;
-  const concat = "'" + url + "'";
+  const data = { id: this.event.target.getAttribute("unique_id"), title: newTitle, thoughts: newThoughts };
 
-  fetch("/posts/_id=" + concat, {
+  fetch('/posts', {
     method: "put",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ data }),
+    body: JSON.stringify(data),
   })
-    .then((response) => response.json())
-    .then((respo) => {
-      window.location.reload(true);
-    });
+  .then((response) => response.json())
+  .then((respo) => {
+    window.location.reload(true);
+  });
 
   /* fetchings data from the surver then uploads the new data
 need to add a prompt to hold the variables to have the user input the information. 
@@ -56,7 +54,21 @@ might need to use the this keyword to get the data gu=
 }
 
 function deleteBlogEntry() {
-  // sends a delete request using the this key to grab the information of the element that called the fucntioned
+  // console.log(this.event.posts)
+  fetch('/posts', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+        id: this.event.target.getAttribute("unique_id")
+    })
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  })
+  .then(response => {
+    window.location.reload(true)
+  })
+  .catch(error => console.error(error))
 }
 
 function serializer() {}
