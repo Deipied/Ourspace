3b8739be874298b36316d0509a3ada94f25869b0
GroovyCj <hairston917@gmail.com>
2022-06-15 14:13:06 -0700

    added weather Api functionality, also styled the card and website

--- public/main.js
+++ public/main.js
@@ -35,17 +35,21 @@ function editBlogEntry(event) {
   const newThoughts = window.prompt("Please enter a new post.");
 
   // functionality for empty input
-  const data = { id: this.event.target.getAttribute("unique_id"), title: newTitle, thoughts: newThoughts };
+  const data = {
+    id: this.event.target.getAttribute("unique_id"),
+    title: newTitle,
+    thoughts: newThoughts,
+  };
 
-  fetch('/posts', {
+  fetch("/posts", {
     method: "put",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   })
-  .then((response) => response.json())
-  .then((respo) => {
-    window.location.reload(true);
-  });
+    .then((response) => response.json())
+    .then((respo) => {
+      window.location.reload(true);
+    });
 
   /* fetchings data from the surver then uploads the new data
 need to add a prompt to hold the variables to have the user input the information. 
@@ -56,20 +60,20 @@ might need to use the this keyword to get the data gu=
 
 function deleteBlogEntry() {
   // console.log(this.event.posts)
-  fetch('/posts', {
-    method: 'delete',
-    headers: { 'Content-Type': 'application/json' },
+  fetch("/posts", {
+    method: "delete",
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-        id: this.event.target.getAttribute("unique_id")
+      id: this.event.target.getAttribute("unique_id"),
+    }),
+  })
+    .then((res) => {
+      if (res.ok) return res.json();
     })
-  })
-  .then(res => {
-    if (res.ok) return res.json()
-  })
-  .then(response => {
-    window.location.reload(true)
-  })
-  .catch(error => console.error(error))
+    .then((response) => {
+      window.location.reload(true);
+    })
+    .catch((error) => console.error(error));
 }
 
 function serializer() {}
@@ -86,3 +90,32 @@ function serializer() {}
 
 //function newDate() {
 // TODO: optional add a place to pla
+
+function getweather() {
+  fetch(
+    "http://api.weatherapi.com/v1/current.json?key=1ab36ecbfc554103bf8192258221506&q=auto:ip&aqi=no"
+  )
+    .then((res) => res.json())
+    .then((data) => renderData(data));
+}
+
+window.addEventListener("load", getweather);
+
+function renderData(data) {
+  wCard = document.createElement("div");
+  const currentWeather = data.current.temp_f;
+  const icon = data.current.condition.icon;
+
+  wCard.innerHTML = `
+  <div class="card" style="width: 8rem;">
+  <img src="${icon}" class="card-img-top" alt="...">
+  <div class="card-body">
+  <p>Current Weather </p>
+    <p class="card-text">${currentWeather}℉</p>
+  </div>
+</div>
+  
+  `;
+
+  document.querySelector("#weather-container").appendChild(wCard);
+}
